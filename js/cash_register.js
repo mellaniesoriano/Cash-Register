01015e0725c4c35ccc4d9973311ebd8f2363f3b5
mellaniesoriano <msoriano808@gmail.com>
2017-05-23 07:57:19 -1000

    made changes to add multiple numbers

--- js/cash_register.js
+++ js/cash_register.js
@@ -7,89 +7,90 @@ var num2 = '';
 // EVERYTHING WILL BE STORED IN HERE
 displayBox.innerHTML = displayNums;
 
+
 // NUMBER BUTTONS - When clicked the number will be displayed in the displayBox div && stored in displayNums. *//
 // ONE
 one.addEventListener('click', number1);
 function number1() {
-  displayNums = '1';
-  displayBox.innerHTML += displayNums;
+  displayNums += '1';
+  displayBox.innerHTML = displayNums;
   num1 += '1';
 }
 // TWO
 two.addEventListener('click', number2);
 function number2() {
-  displayNums = '2';
-  displayBox.innerHTML += displayNums;
+  displayNums += '2';
+  displayBox.innerHTML = displayNums;
   num1 += '2';
 }
 // THREE
 three.addEventListener('click', number3);
 function number3() {
-  displayNums = '3';
-  displayBox.innerHTML += displayNums;
+  displayNums += '3';
+  displayBox.innerHTML = displayNums;
   num1 += '3';
 }
 // FOUR
 four.addEventListener('click', number4);
 function number4() {
-  displayNums = '4';
-  displayBox.innerHTML += displayNums;
+  displayNums += '4';
+  displayBox.innerHTML = displayNums;
   num1 += '4';
 }
 // FIVE
 five.addEventListener('click', number5);
 function number5() {
-  displayNums = '5';
-  displayBox.innerHTML += displayNums;
+  displayNums += '5';
+  displayBox.innerHTML = displayNums;
   num1 += '5';
 }
 // SIX
 six.addEventListener('click', number6);
 function number6() {
-  displayNums = '6';
-  displayBox.innerHTML += displayNums;
+  displayNums += '6';
+  displayBox.innerHTML = displayNums;
   num1 += '6';
 }
 // SEVEN
 seven.addEventListener('click', number7);
 function number7() {
-  displayNums = '7';
-  displayBox.innerHTML += displayNums;
+  displayNums += '7';
+  displayBox.innerHTML = displayNums;
   num1 += '7';
 }
 // EIGHT
 eight.addEventListener('click', number8);
 function number8() {
-  displayNums = '8';
-  displayBox.innerHTML += displayNums;
+  displayNums += '8';
+  displayBox.innerHTML = displayNums;
   num1 += '8';
 }
 // NINE
 nine.addEventListener('click', number9);
 function number9() {
-  displayNums = '9';
-  displayBox.innerHTML += displayNums;
+  displayNums += '9';
+  displayBox.innerHTML = displayNums;
   num1 += '9';
 }
 // ZERO
 zero.addEventListener('click', number0);
 function number0() {
-  displayNums = '0';
-  displayBox.innerHTML += displayNums;
+  displayNums += '0';
+  displayBox.innerHTML = displayNums;
   num1 += '0';
 }
 // DOUBLE ZERO
 doubleZero.addEventListener('click', numberDblZero);
 function numberDblZero() {
-  displayNums = '00';
-  displayBox.innerHTML += displayNums;
+  displayNums += '00';
+  displayBox.innerHTML = displayNums;
   num1 += '00';
 }
 // DECIMAL POINT
 decimal.addEventListener('click', decimalPoint);
 function decimalPoint() {
-  displayNums = '.';
-  displayBox.innerHTML += displayNums;
+  displayNums += '.';
+  displayBox.innerHTML = displayNums;
   num1 += '.';
 }
 
@@ -186,5 +187,6 @@ function depositGalleons() {
 function withdrawGalleons() {
   displayNums = myCalculator.deleteMemory();
   displayBox.innerHTML = '';
+  console.log(displayNums);
 }
 
